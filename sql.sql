cceab40c50242c0cf9c0d62c28625a34b447a0f2
f4Lq <167790412+f4Lq@users.noreply.github.com>
2024-04-22 18:02:16 +0200

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,8 +1,8 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES 
-('silencer', 'Silencer', 2, 0, 1, 0),
-('grip', 'Grip', 2, 0, 1, 0),
-('flashlight', 'Flashlight', 2, 0, 1, 0),
-('scope', 'Scope', 2, 0, 1, 0),
-('box_magazine', 'Box Magazine', 2, 1, 1, 0),
-('extended_clip', 'Extended Clip', 2, 0, 1, 0)
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES 
+('suppresor', 'Suppresor', 2, 0, 1),
+('grip', 'Grip', 2, 0, 1),
+('flashlight', 'Flashlight', 2, 0, 1),
+('scope', 'Scope', 2, 0, 1),
+('box_magazine', 'Box Magazine', 2, 1, 1),
+('extended_clip', 'Extended Clip', 2, 0, 1)
 ;
